04e1777dde5c422654ca19e1f34840e4715d36df
Nicolas Guerrero <saynaysurf@gmail.com>
2020-01-29 12:03:40 -0800

    Created routes in app.py, User class in models.py, and template html files.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, redirect, render_template
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -10,3 +10,39 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+from flask_debugtoolbar import DebugToolbarExtension
+app.config['SECRET_KEY'] = "secret"
+debug = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def start():
+    return redirect("/users")
+
+@app.route("/users")
+def show_user_list():
+    # render user list and add user button
+
+@app.route("/users/new")
+    # render new user form 
+
+@app.route("users/new", methods=["POST"])
+    # create new user in database from form inputs
+    # redirect to user list '/users'
+
+@app.route("user/<int:user_id>")
+    # render user_id detail page
+    # button to edit or delete
+
+@app.route("user/<int:user_id>/edit")
+    # render the edit form
+    # 
+
+@app.route("user/<int:user_id>/edit", methods=["POST"])
+    # edit the user in the database
+    # redirect to all users list '/users'
+
+@app.route("user/<int:user_id>/delete", methods=["POST"])
+    # delete the user in the database
+    # redirect to '/users'
--- models.py
+++ models.py
@@ -1,1 +1,31 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database. """
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """User."""
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    first_name = db.Column(db.String(15),
+                           nullable=False)
+    last_name = db.Column(db.String(25),
+                          nullable=False)
+    image_url = db.Column(db.String(512),
+                          nullable=False,
+                          default='https://static1.squarespace.com/static/54b7b93ce4b0a3e130d5d232/54e20ebce4b014cdbc3fd71b/5a992947e2c48320418ae5e0/1519987239570/icon.png')
+
+    def __repr__(self):
+        """ Show info about user."""
+
+        user = self
+        return f"<User {user.id}: {user.first_name} {user.last_name}>"
